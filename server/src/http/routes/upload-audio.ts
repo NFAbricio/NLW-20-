e1df36b8d8405fcc5cbd57467adaf334496e059b
NFAbricio <fabnunes014@gmail.com>
2025-07-24 15:31:42 -0300

    feat(http): add audio upload endpoint

---
+++ server/src/http/routes/upload-audio.ts
@@ -0,0 +1,17 @@
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod"
+import { z } from "zod/v4"
+export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
+	app.post(
+		"/rooms/:roomId/audio",
+		{
+			schema: {
+				params: z.object({
+					roomId: z.string(),
+				}),
+			},
+		},
+		(request, reply) => {
+			const { roomId } = request.params
+		},
+	)
+}
